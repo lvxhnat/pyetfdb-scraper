867f427f20e51e6c5fcfeb6d4be3433830ce7dde
yikuang <yikuang@synthesis.partners>
2023-02-04 21:39:07 +0800

    fix: json file excluded in pypi package

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     url="https://github.com/lvxhnat/pyetf-scraper",
     package_dir={"": "src"},
     packages=find_packages("src", exclude=["*tests"]),
-    package_data={"my_package": ["data/etfdb.json"]},
+    package_data={name: ["data/etfdb.json"]},
     python_requires=">=3.7",
     install_requires=[
         "requests",
